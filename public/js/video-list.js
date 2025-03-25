1db0994099c1d055d67a22ad2a2e4c64aba03a6f
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-03-25 18:52:53 +0100

    feat: implement video-list functionality with dynamic video loading and styling

--- public/js/video-list.js
+++ public/js/video-list.js
@@ -1,5 +1,79 @@
 (
     () => {
         // alert('video-list.js loaded')
+        let counter=1
+const container = document.createElement("div")
+container.classList.add("container-fluid")
+container.classList.add("p-5")
+fetch("/test")
+.then(response => response.json())
+.then(data => {
+    document.body.appendChild(container);
+    
+    for (let movie_data of data) {
+        createRow(movie_data);
+    }
+    addEventToVideos();
+});
+const createVideo = (movie_data) => {
+    const type = movie_data.type
+    const id = movie_data.ID
+    const element = document.createElement("video");
+    element.src = `/video/${movie_data.name.split(".")[0]}`;
+    element.controls = false;
+    element.muted = true;
+    element.dataset.name=movie_data.name
+    return element;
+}
+const createTitle = (movie_data) => {
+    const element = document.createElement("h1");
+    element.textContent = movie_data.type;
+    document.body.appendChild(element);
+    return element;
+}
+
+const createRow = (movie_data) => {
+    const title = createTitle(movie_data);
+    const titleCol = document.createElement("div");
+    titleCol.classList.add("col-md-12");
+    const row = document.createElement("div");
+    titleCol.appendChild(title);
+    row.appendChild(titleCol);
+    if(counter%2==0){
+        row.classList.add("colored")
+    }
+
+    const videoCol = document.createElement("div");
+    videoCol.classList.add("col-md-12");
+    row.appendChild(videoCol);
+
+    for (let movie of movie_data.movies) {
+        let video = createVideo(movie);
+        videoCol.appendChild(video);
+    }
+    row.classList.add("row");
+    container.appendChild(row);
+    counter++
+}
+
+const addEventToVideos = () => {
+    const video_array = document.getElementsByTagName("video");
+
+    for (let video of video_array) {
+
+        video.addEventListener("mouseenter", () => {
+            video.play();
+        });
+
+        video.addEventListener("mouseleave", () => {
+            video.currentTime = 0;//rewind video to start
+            video.pause();
+        });
+
+        video.addEventListener("click", () => {
+            window.location.href = "/";
+        });
+    }
+}
     }
 )()
--- routes.js
+++ routes.js
@@ -4,8 +4,83 @@ const path = require('path');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const db = require('./db');
+
+
+const convertResult = (results) => {
+    const movie_data = [];
+    for (let movie of results) {
+      if (movie_data.filter(mov => mov.type === movie.type).length === 0) {
+        movie_data.push({
+          type: movie.type,
+          movies: []
+        })
+      }
+  
+      for (let mov of movie_data) {
+        if (mov.type === movie.type) {
+          mov.movies.push({
+            ID: movie.ID,
+            name: movie.name,
+            description: movie.description,
+            type: movie.type
+          });
+        }
+      }
+  
+    }
+    return movie_data;
+  }
+
+  
 module.exports = (app) => {
 
+    app.get("/test", (req, res) => {
+        const queryString = 'SELECT * FROM `videos`';
+        db.query(queryString, (err, results) => {
+          if (err) throw err;
+          const movie_data = convertResult(results);
+          res.status(200);  
+          res.send(JSON.stringify(movie_data));
+        }
+        )
+        
+      });
+      
+  app.get("/video/:name", (req, res) => {
+    const videoName = req.params.name;
+    const videoPath = path.join(__dirname, 'videos', `${videoName}.mp4`);
+    const stat = fs.statSync(videoPath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if (range) {
+      // Handle range request (partial content)
+      const parts = range.replace(/bytes=/, '').split('-');
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+      const chunkSize = end - start + 1;
+
+      const fileStream = fs.createReadStream(videoPath, { start, end });
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunkSize,
+        'Content-Type': 'video/mp4',
+      };
+
+      res.writeHead(206, head);
+      fileStream.pipe(res);
+    } else {
+      // Send full video if no range is specified
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      };
+
+      res.writeHead(200, head);
+      fs.createReadStream(videoPath).pipe(res);
+    }
+  });
     // Register route
     app.post('/register', async (req, res) => {
         try {
